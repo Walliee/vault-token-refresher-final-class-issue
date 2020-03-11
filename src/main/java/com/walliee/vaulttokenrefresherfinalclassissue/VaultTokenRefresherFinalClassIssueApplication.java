8753a62ade2690f3aca1dd54c38fb0a916929bee
Walliee <ansh.mehra@me.com>
2020-03-11 18:06:25 -0400

    Initial commit

---
+++ src/main/java/com/walliee/vaulttokenrefresherfinalclassissue/VaultTokenRefresherFinalClassIssueApplication.java
@@ -0,0 +1,14 @@
+package com.walliee.vaulttokenrefresherfinalclassissue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+public class VaultTokenRefresherFinalClassIssueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaultTokenRefresherFinalClassIssueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/walliee/vaulttokenrefresherfinalclassissue/VaultTokenRefresherFinalClassIssueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walliee.vaulttokenrefresherfinalclassissue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultTokenRefresherFinalClassIssueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
